305f0fd17a3b4c34360e6542c6ee181bc22274e2
giancode1 <gieg5c@gmail.com>
2021-04-11 17:59:43 -0500

    first commit

---
+++ juego_ahorcado.py
@@ -0,0 +1,166 @@
+# -*- coding: utf-8 -*-
+"""
+@author: Giancarlo Culcay
+"""
+
+import random
+import os
+
+ganaste = ['''
+   __ _  __ _ _ __   __ _ ___| |_ ___ 
+ / _` |/ _` | '_ \ / _` / __| __/ _ \
+| (_| | (_| | | | | (_| \__ \ ||  __/
+ \__, |\__,_|_| |_|\__,_|___/\__\___|
+ |___/                               
+''']
+
+IMAGES = ['''           
+    +---+
+    |   |
+        |
+        |
+        |
+        |
+    =========''', '''
+    +---+
+    |   |
+    O   |
+        |
+        |
+        |
+    =========''', '''
+    +---+
+    |   |
+    O   |
+    |   |
+        |
+        |
+    =========''', '''
+    +---+
+    |   |
+    O   |
+   /|   |
+        |
+        |
+    =========''', '''
+    +---+
+    |   |
+    O   |
+   /|\  |
+        |
+        |
+    =========''', '''
+    +---+
+    |   |
+    O   |
+   /|\  |
+    |   |
+        |
+    =========''', '''
+    +---+
+    |   |
+    O   |
+   /|\  |
+    |   |
+   /    |
+        |
+    =========''', '''
+    +---+
+    |   |
+    O   |
+   /|\  |
+    |   |
+   / \  |
+        |
+    =========''']
+
+palabras = []
+intentos = 7
+aciertos= 0
+errores = 0
+z = 0
+
+def normalizar(s): # Quita tildes
+    replacements = (
+        ("á", "a"),
+        ("é", "e"),
+        ("í", "i"),
+        ("ó", "o"),
+        ("ú", "u"),
+    )
+    for a, b in replacements:
+        s = s.replace(a, b).replace(a.upper(), b.upper())
+    return s
+
+def palabra_random():
+    with open("./archivos/data.txt","r", encoding="utf-8") as fichero:
+        for linea in fichero:
+            linea_formateada = linea.replace("\n","")
+            palabras.append(linea_formateada)
+    return normalizar((random.choice(palabras)))
+
+
+def presentacion():
+    os.system ("cls") 
+    texto_inicial = " Juego del Ahorcado "
+    texto_presentar = texto_inicial.center(50,'-') 
+    print(texto_presentar,"\n")
+    print("=> Adivina la palabra :")
+    print(IMAGES[errores])
+    f_mostrar(palabra_oculta)
+    print("intentos: ",intentos)
+    print("aciertos: ",aciertos)
+    print("errores:  ",errores)
+
+def f_mostrar(x):
+    mostrar = "".join(x) 
+    print(mostrar,'\n')
+
+def verificar(letra):
+    if len(letra) == 0 and letra.isalpha() :
+        errores +=1
+
+os.system ("cls") 
+palabra = palabra_random()
+palabra_oculta = ["_"]*len(palabra)
+
+
+while True:
+    try:
+        presentacion()
+        #print(palabra)   #respuesta, comentar o eliminar
+        l = input("Ingresa una letra: ")
+        verificar(l)
+
+        if l in palabra_oculta :
+            errores +=1
+            print("Ya ingresaste esta letra")
+        else:
+            for index,x in enumerate(palabra):
+                if x == l:
+                    palabra_oculta[index] = l
+                    aciertos += 1
+
+        if palabra.find(l) == -1:   #sino encuentra, devuelve -1
+            errores +=1
+
+        f_mostrar(palabra_oculta)
+        presentacion()
+        mipalabra = "".join(palabra_oculta)  
+        
+        if errores >= intentos:
+            print("Has perdido :(")
+            print(f'La palabra era: {palabra}')
+            break
+
+        if mipalabra == palabra :
+            #print(ganaste[0])
+            print("Felicidades Ganaste!! :D")
+            break
+
+    except ValueError:
+        print("Porfavor ingresa solo una letra")
+        #print("Ya has ingresado!")
+    except IndexError:
+        print("Fin")
+ 
